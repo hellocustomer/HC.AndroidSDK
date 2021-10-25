dd5e3b30ade5d9463322f9d3da6c78c288f8ba29
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2021-10-25 14:20:57 +0200

    docs: add documentation

--- sdk/src/main/java/com/hellocustomer/sdk/dialog/HelloCustomerDialog.kt
+++ sdk/src/main/java/com/hellocustomer/sdk/dialog/HelloCustomerDialog.kt
@@ -4,9 +4,23 @@ import androidx.fragment.app.FragmentManager
 
 public interface HelloCustomerDialog {
 
+    /**
+     * Shows HelloCustomer dialog.
+     *
+     * @param fragmentManager manager to show and maintain dialog
+     * @param tag dialog fragment's TAG
+     */
     public fun show(fragmentManager: FragmentManager, tag: String): HelloCustomerDialog
 
+    /**
+     * Shows HelloCustomer dialog.
+     *
+     * @param fragmentManager manager to show and maintain dialog
+     */
     public fun show(fragmentManager: FragmentManager): HelloCustomerDialog
 
+    /**
+     * Dismisses HelloCustomer dialog.
+     */
     public fun dismissDialog(): HelloCustomerDialog
 }
